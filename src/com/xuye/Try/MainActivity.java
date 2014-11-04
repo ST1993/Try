1547afa35e06a9bcd29ab56f32452b26bf550cfb
xuye <605697031@qq.com>
2014-11-04 14:48:26 +0800

    https://github.com/ST1993/Try.git

---
+++ src/com/xuye/Try/MainActivity.java
@@ -0,0 +1,81 @@
+package com.xuye.Try;
+
+import java.util.HashMap;
+import java.util.Random;
+
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
+import cn.smssdk.gui.RegisterPage;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	private static final String[] AVATARS = {
+			"http://tupian.qqjay.com/u/2011/0729/e755c434c91fed9f6f73152731788cb3.jpg",
+			"http://99touxiang.com/public/upload/nvsheng/125/27-011820_433.jpg",
+			"http://img1.touxiang.cn/uploads/allimg/111029/2330264224-36.png",
+			"http://img1.2345.com/duoteimg/qqTxImg/2012/04/09/13339485237265.jpg",
+			"http://diy.qqjay.com/u/files/2012/0523/f466c38e1c6c99ee2d6cd7746207a97a.jpg",
+			"http://img1.touxiang.cn/uploads/20121224/24-054837_708.jpg",
+			"http://img1.touxiang.cn/uploads/20121212/12-060125_658.jpg",
+			"http://img1.touxiang.cn/uploads/20130608/08-054059_703.jpg",
+			"http://diy.qqjay.com/u2/2013/0422/fadc08459b1ef5fc1ea6b5b8d22e44b4.jpg",
+			"http://img1.2345.com/duoteimg/qqTxImg/2012/04/09/13339510584349.jpg",
+			"http://img1.touxiang.cn/uploads/20130515/15-080722_514.jpg",
+			"http://diy.qqjay.com/u2/2013/0401/4355c29b30d295b26da6f242a65bcaad.jpg" };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		SMSSDK.initSDK(this, "40cf78437636", "b59411d07fa5460a8e0112f3b28fc5a9");
+		RegisterPage registerPage = new RegisterPage();
+
+		registerPage.setRegisterCallback(new EventHandler() {
+			public void afterEvent(int event, int result, Object data) {
+				// 解析注册结果
+				if (result == SMSSDK.RESULT_COMPLETE) {
+					@SuppressWarnings("unchecked")
+					HashMap<String, Object> phoneMap = (HashMap<String, Object>) data;
+					String country = (String) phoneMap.get("country");
+					String phone = (String) phoneMap.get("phone");
+
+					// 提交用户信息
+					registerUser(country, phone);
+				}
+			}
+		});
+		registerPage.show(this);
+	}
+
+	private void registerUser(String country, String phone) {
+		Random rnd = new Random();
+		int id = Math.abs(rnd.nextInt());
+		String uid = String.valueOf(id);
+		String nickName = "SmsSDK_User_" + uid;
+		String avatar = AVATARS[id % 12];
+		SMSSDK.submitUserInfo(uid, nickName, avatar, country, phone);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
